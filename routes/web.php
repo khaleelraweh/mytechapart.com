1b3757bb37674bfae62e53efe0fe594f7c255076
khaleelraweh <khaleelvisa@gmail.com>
2026-03-13 04:03:01 +0300

    save all

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('frontend.index');
-    // return view('auth.login');
+    // return view('frontend.index');
+    return view('auth.login');
 });
 
 Auth::routes();
