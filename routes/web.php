04e1a71880cc581893eaa559bab77f1f970fc791
khaleelraweh <khaleelvisa@gmail.com>
2026-03-14 16:48:59 +0300

    add backend controller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,21 @@
 <?php
 
+use App\Http\Controllers\Backend\BackendController;
 use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('frontend.index');
-    // return view('auth.login');
-});
+// Route::get('/', function () {
+//     return view('frontend.index');
+//     // return view('auth.login');
+// });
 
 Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
+Route::get('/admin/index', [BackendController::class, 'index'])->name('backend.index');
+Route::get('/admin/login', [BackendController::class, 'login'])->name('backend.login');
 
-Route::get('/admin/index', function () {
-    return view('backend.index');
-    // return view('auth.login');
-});
 
 
 
